9178144a5fd9c23d272b259017efef8ce3ca17fa
Pamali-Ranasinghe <sapunikasr@gmail.com>
2026-03-22 02:38:55 +0530

    Add SMTP and mongodb configurations

--- apps/notify-service/notify/src/main/java/com/example/notify/Application.java
+++ apps/notify-service/notify/src/main/java/com/example/notify/Application.java
@@ -2,6 +2,8 @@ package com.example.notify;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
 public class Application {
@@ -9,5 +11,10 @@ public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
+
+	@Bean
+    public WebClient.Builder webClientBuilder() {
+        return WebClient.builder();
+    }
 
 }
---
+++ apps/notify-service/notify/src/main/java/com/example/notify/TestNotificationClient.java
@@ -0,0 +1,35 @@
+// import org.springframework.boot.CommandLineRunner;
+// import org.springframework.stereotype.Component;
+
+// @Component
+// public class TestNotificationClient implements CommandLineRunner{
+
+//     private final WebClient webClient;
+
+//     public TestNotificationClient(WebClient.Builder builder) {
+//         this.webClient = builder.baseUrl("http://localhost:8080").build(); // Adjust to your notification service port
+//     }
+
+//     @Override
+//     public void run(String... args) {
+
+//         // Map<String, Object> payload = new HashMap<>();
+//         // payload.put("recipientEmail", "pamalisr@gmail.com");
+//         // payload.put("recipientPhone", "0702399699");
+//         // payload.put("orderId", "0021");
+//         // payload.put("sendEmail", "true");
+//         // payload.put("sendSMS", "true");
+
+//         // webClient.post()
+//         //         .uri("/api/v1/notifications/customer/order-confirmed") // your actual controller path
+//         //         .contentType(MediaType.APPLICATION_JSON)
+//         //         .bodyValue(payload)
+//         //         .retrieve()
+//         //         .bodyToMono(String.class)
+//         //         .subscribe(response -> System.out.println("✅ Response from Notification Service: " + response),
+//         //                    error -> System.err.println("❌ Error: " + error.getMessage()));
+//     }
+
+
+    
+// }
