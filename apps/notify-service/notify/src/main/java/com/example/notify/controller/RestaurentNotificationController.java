0894d665b68759da351d2540d116e5032fc5eb23
Pamali-Ranasinghe <sapunikasr@gmail.com>
2026-03-22 02:29:58 +0530

    Add controllers for all users

---
+++ apps/notify-service/notify/src/main/java/com/example/notify/controller/RestaurentNotificationController.java
@@ -0,0 +1,41 @@
+package com.example.notify.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import com.example.notify.model.Notification;
+import com.example.notify.service.RestaurantNotificationService;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:3000")
+@RequestMapping("/api/v1/notifications/restaurent")
+public class RestaurentNotificationController {
+
+    private RestaurantNotificationService restaurantNotificationService;
+
+    @Autowired
+    public RestaurentNotificationController(RestaurantNotificationService restaurantNotificationService){
+        this.restaurantNotificationService = restaurantNotificationService;
+    }
+    
+
+    @PostMapping("/new-order")
+    public ResponseEntity<Notification> RestaurentNewOrderNotification(@RequestBody Notification notification) {
+        Notification savedNotification = restaurantNotificationService.RestaurentNewOrderNotification(notification);
+        return ResponseEntity.ok(savedNotification);
+    }
+
+    @PostMapping("/order-accepted")
+    public ResponseEntity<Notification> RestaurantOrderAcceptedNotification(@RequestBody Notification notification) {
+        Notification savedNotification = restaurantNotificationService.RestaurantOrderAcceptedNotification(notification);
+        return ResponseEntity.ok(savedNotification);
+    }
+
+    @PostMapping("/order-cancelled")
+    public ResponseEntity<Notification> RestaurantOrderCancelledNotification(@RequestBody Notification notification) {
+        Notification savedNotification = restaurantNotificationService.RestaurantOrderCancelledNotification(notification);
+        return ResponseEntity.ok(savedNotification);
+    }
+
+}
