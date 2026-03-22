f19eae37fd863a0c9c8296ea2b6eac77fc851225
Pamali-Ranasinghe <sapunikasr@gmail.com>
2026-03-22 13:35:27 +0530

    Updated application properties

--- apps/notify-service/notify/src/main/java/com/example/notify/Application.java
+++ apps/notify-service/notify/src/main/java/com/example/notify/Application.java
@@ -3,7 +3,7 @@ package com.example.notify;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.reactive.function.client.WebClient;
+// import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
 public class Application {
@@ -12,9 +12,9 @@ public class Application {
 		SpringApplication.run(Application.class, args);
 	}
 
-	@Bean
-    public WebClient.Builder webClientBuilder() {
-        return WebClient.builder();
-    }
+	// @Bean
+    // public WebClient.Builder webClientBuilder() {
+    //     return WebClient.builder();
+    // }
 
 }
